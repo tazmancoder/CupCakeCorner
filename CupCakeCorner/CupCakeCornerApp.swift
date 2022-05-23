edca12790dd8275ef325397f49212fe66de83efe
Mark J Perryman <appledad05@gmail.com>
2022-05-23 09:29:13 -0600

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Mark Perryman on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
